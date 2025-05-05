101b525956d6329b35c55d0c367b28452f1d2ff8
Akash <hafejmohammadakash@gmail.com>
2025-05-05 11:29:17 +0600

    json file added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,28 @@
-import React from 'react';
-import Banner from '../components/Banner';
+import React from "react";
+import Banner from "../components/Banner";
 
 const Home = () => {
-    return (
-        <div>
-            {/* Banner */}
-            <Banner />
+  return (
+    <div>
+      {/* Banner */}
+      <Banner />
+
+      {/* Head Line */}
+      <div className="my-20">
+        <h1 className="text-center text-4xl font-bold">Our All Treatments</h1>
+        <p className="text-center mt-3 text-gray-400 w-2/5 mx-auto">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+          distinctio, natus nulla et ea provident voluptas? Ea consequatur
+          officia quas ab animi minus ut?
+        </p>
+      </div>
+
+      {/* All Treatment cards */}
 
 
-            {/*  */}
 
-
-            {/*  */}
-        </div>
-    );
+    </div>
+  );
 };
 
 export default Home;
